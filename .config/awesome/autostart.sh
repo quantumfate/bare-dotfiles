b25888e816a88ca630f9e2f795298eccc1de81b6
Leon Connor Holm <leonconnorholm@gmail.com>
2022-12-24 02:47:09 +0100

    autostart/pdf

--- .config/awesome/autostart.sh
+++ .config/awesome/autostart.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-
-function run {
-  if ! pgrep $1 ;
-  then
-    $@&
+function run() {
+  # check if the program is installed and executable
+  if which "$1" > /dev/null; then
+    # check if the program is already running
+    if ! pgrep -f "$1" > /dev/null; then
+      # execute the program if it is not running
+      eval "$@" &
+    fi
+  else
+    # output an error message if the program is not installed or executable
+    echo "Error: program $1 is not installed or executable."
   fi
 }
 #run xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #run xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #autorandr horizontal
 run nm-applet
+run redshift -l 53.55073:9.99302
 run xfce4-clipman
 #run caffeine
 run pamac-tray
@@ -22,9 +29,9 @@ run volumeicon
 #run nitrogen --restore
 run conky -c $HOME/.config/awesome/system-overview
 #you can set wallpapers in themes as well
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
+run feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
 run udiskie -a -s --terminal=Alacrityy
-run $HOME/.scripts/pdfwatcher/pdfwatcher.sh
+
 #run applications from startup
 #run firefox
 #run atom
